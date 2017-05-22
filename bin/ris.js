9caf7d0686fbae0bde4093f1ec52c179e70a2af0
dvpetrov <dvpetrov.main@gmail.com>
2017-05-22 13:30:35 +0300

    scan lerna.json for package paths, work with alias

--- bin/ris.js
+++ bin/ris.js
@@ -2,6 +2,7 @@
 'use strict';
 
 const spawn = require('cross-spawn')
+const fs = require('fs')
 const glob = require('glob')
 const path = require('path')
 
@@ -9,50 +10,91 @@ const packageName = process.argv[2]
 const command = process.argv[3]
 const args = process.argv.slice(4)
 
-const packageNames = glob.sync('./packages/*').reduce((result, packageFolder) => {
-  const packageJson = require(`../${packageFolder}/package.json`)
-  const rispaName = packageJson['rispa:name']
-  if (packageName === 'all' && !(packageJson['scripts'] && command in packageJson['scripts'])) {
+const lernaJson = requireIfExist("../lerna.json");
+
+const packagePaths = ['node_modules/*'].concat(lernaJson && lernaJson.packages ? lernaJson.packages : [])
+
+const packages = packagePaths.map(packagePath =>
+  glob.sync(packagePath).reduce((result, packageFolder) => {
+    const packageJson = requireIfExist(`../${packageFolder}/package.json`)
+    if (!packageJson || !packageJson['rispa:plugin']) {
+      return result
+    }
+
+    const rispaName = packageJson['rispa:name']
+    if (packageName === 'all' && !(packageJson.scripts && command in packageJson.scripts)) {
+      return result
+    }
+
+    const packageInfo = {
+      path: packageFolder,
+      alias: rispaName,
+      name: packageJson['name']
+    }
+
+    if (rispaName) {
+      result[rispaName] = packageInfo
+    }
+
+    result[packageInfo.name] = packageInfo
+
     return result
-  }
-  if (rispaName) {
-    result[rispaName] = packageFolder
-  } else {
-    result[packageJson['name']] = packageFolder
-  }
+  }, {})
+).reduce((result, currentResult) => Object.assign(result, currentResult))
 
-  return result
-}, {})
+if (packages !== 'all' && !packages[packageName]) {
+  console.log(`Can't find package with name: ${packageName}.\n\nList of available packages:`)
+  printPackages(packages);
 
-if (packageName !== 'all' && !packageNames[packageName]) {
-  console.log(`Can't find package with name: ${packageName}.\n\nList of available packages:\n - ${Object.keys(packageNames).join('\n - ')}`)
   process.exit(1)
 }
 
-
-const result = callScript(packageName, packageNames, command, args)
+const result = callScript(packageName, packages, command, args)
 process.exit(result)
 
-function callScript(packageName, packageNames, command, args){
+function printPackages(packages) {
+  Object.keys(packages).forEach((key) => {
+    const { alias, name } = packages[key];
+
+    if (alias === key) {
+      console.log(` - ${alias} - alias for ${name}`)
+    } else {
+      console.log(` - ${name}`);
+    }
+  });
+}
+
+function callScript(packageName, packages, command, args) {
   if (packageName === 'all') {
-    return Object.values(packageNames).reduce( (result, path) => {
+    return Object.values(packages).reduce((result, packageInfo) => {
       const res = spawn.sync(
-       'yarn',
-       [command].concat(args),
-       {
-         cwd: path,
-         stdio: 'inherit'
-       })
-       return res.status || result
-   }, 0 )
-   } else {
-     return spawn.sync(
+        'yarn',
+        [command].concat(args),
+        {
+          cwd: packageInfo.path,
+          stdio: 'inherit',
+        })
+      return res.status || result
+    }, 0)
+  } else {
+    return spawn.sync(
       'yarn',
       [command].concat(args),
       {
-        cwd: packageNames[packageName],
-        stdio: 'inherit'
+        cwd: packages[packageName].path,
+        stdio: 'inherit',
       }
     ).status
-   }
+  }
+}
+
+function requireIfExist(id) {
+  try {
+    return require(id)
+  } catch (e) {
+    if (e instanceof Error && e.code === "MODULE_NOT_FOUND") {
+      return null
+    }
+    throw e;
+  }
 }
