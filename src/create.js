0a5ad0d9b29d637686954f0cae0dd4722c7b584a
dvpetrov <dvpetrov.main@gmail.com>
2017-05-23 16:22:40 +0300

    generate project structure

--- src/create.js
+++ src/create.js
@@ -1,22 +1,43 @@
 'use strict';
 
+const fs = require('fs')
+const path = require('path')
+const spawn = require('cross-spawn')
 const { prompt } = require('inquirer')
+const nodePlop = require('node-plop')
 
-const { handleError } = require('./core');
-const githubApi = require('./githubApi');
+const { handleError } = require('./core')
+const githubApi = require('./githubApi')
+const { cloneRepository } = require('./git')
+
+const BASE_PATH = process.cwd()
+const GENERATORS_PATH = path.resolve(__dirname, '../generators/index.js')
 
 async function create(projectName, ...args) {
   try {
     if (!projectName) {
-      projectName = (await enterProjectName()).projectName;
+      projectName = (await enterProjectName()).projectName
     }
+
+    projectName = projectName.replace(/\s+/g, '-').toLowerCase();
 
     const { data: { items: plugins } } = await githubApi.plugins();
 
-    const { installPlugins } = await selectInstallPlugins(plugins);
+    const { installPluginsNames } = await selectInstallPlugins(plugins);
 
-    console.log('projectName', projectName);
-    console.log('installPlugins', installPlugins);
+    const plop = nodePlop(GENERATORS_PATH)
+    const results = await plop.getGenerator('project').runActions({ projectName })
+
+    const pluginsPath = path.resolve(BASE_PATH, `./${projectName}/packages`)
+
+    if (!fs.existsSync(pluginsPath)) {
+      fs.mkdirSync(pluginsPath)
+    }
+
+    installPlugins(installPluginsNames, plugins, pluginsPath)
+
+    console.log(`Project "${projectName}" successfully generated!`)
+    process.exit(1)
   } catch (e) {
     handleError(e);
   }
@@ -34,9 +55,18 @@ function selectInstallPlugins(plugins) {
   return prompt([{
     type: 'checkbox',
     message: 'Select install plugins:',
-    name: 'installPlugins',
+    name: 'installPluginsNames',
     choices: plugins
   }])
+}
+
+function installPlugins(pluginsNames, plugins, path) {
+  plugins.filter(({ name }) => pluginsNames.indexOf(name) !== -1)
+    .forEach(({ name, clone_url: cloneUrl }) => {
+      console.log(`Install plugin: ${name}`)
+
+      cloneRepository(cloneUrl, path)
+    })
 }
 
 module.exports = create;
