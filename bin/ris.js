f0f00aa7b65c57e6da4e23231e39d54dbce38dbf
Andrew Laiff <q.laiff@gmail.com>
2017-05-23 19:57:57 +0500

    Update executable flag

--- bin/ris.js
+++ bin/ris.js
@@ -1,16 +1,15 @@
 #!/usr/bin/env node
-'use strict';
 
 const commands = {
-  'run': require('../src/run'),
-  'new': require('../src/create')
+  run: require('../src/run'),
+  new: require('../src/create'),
 }
 
-const command = process.argv[2].toLowerCase();
+const command = process.argv[2].toLowerCase()
 const args = process.argv.slice(3)
 
 if (command in commands) {
   commands[command](...args)
 } else {
-  commands.run(...process.argv.slice(2));
+  commands.run(...process.argv.slice(2))
 }
