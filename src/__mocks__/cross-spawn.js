cc7ca13ccbcfb5154996548b90960842df8c57a8
Vladimir <vratsev@gmail.com>
2017-06-14 09:17:19 +0500

    Add tests for commit command

--- src/__mocks__/cross-spawn.js
+++ src/__mocks__/cross-spawn.js
@@ -1,13 +1,19 @@
 const spawn = jest.genMockFromModule('cross-spawn')
 
-function sync(command, args, options) {
-  return {
-    status: typeof command === 'string'
-      && Array.isArray(args)
-      && typeof options === 'object' ? 0 : 1,
-  }
-}
+let mockOutput
+const defaultOutput = [null, new Buffer(''), new Buffer('')]
+
+const sync = jest.fn((command, args, options) => ({
+  status: typeof command === 'string'
+    && Array.isArray(args)
+    && typeof options === 'object' ? 0 : 1,
+  output: mockOutput || defaultOutput,
+}))
 
 spawn.sync = sync
+
+spawn.setMockOutput = newMockOutput => {
+  mockOutput = newMockOutput || defaultOutput
+}
 
 module.exports = spawn
