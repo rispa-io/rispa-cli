12e0608b6ec5b5c0a646757682c2f35b1e7b5279
Vladimir Ratsev <vratsev@gmail.com>
2017-06-27 16:36:16 +0500

    Refactor tests for assemble command

--- src/commands/__tests__/assemble.test.js
+++ src/commands/__tests__/assemble.test.js
@@ -1,73 +1,67 @@
 jest.mock('fs-extra')
 jest.mock('../../tasks/readProjectConfiguration')
 jest.mock('../../tasks/restorePlugin')
+jest.mock('../../tasks/cleanCache')
 jest.mock('../../utils/git')
 
 const readProjectConfigurationTask = require.requireMock('../../tasks/readProjectConfiguration')
 const createRestorePluginTask = require.requireMock('../../tasks/restorePlugin')
+const cleanCache = require.requireMock('../../tasks/cleanCache')
 const mockGit = require.requireMock('../../utils/git')
 
 const AssembleCommand = require.requireActual('../assemble')
 
+const restorePluginTask = jest.fn()
+
+const runCommand = () => {
+  const command = new AssembleCommand({
+    renderer: 'silent',
+  })
+  command.init()
+  return command.run({
+    cwd: '/cwd',
+    projectPath: '/cwd',
+  })
+}
+
 describe('assemble command', () => {
-  beforeEach(() => {
-    jest.resetAllMocks()
-    jest.resetModules()
+  it('should init successfully', () => {
+    const command = new AssembleCommand()
+    command.init()
+    expect(command.tasks.length).toBe(4)
   })
 
-  describe('init', () => {
-    it('should add tasks', () => {
-      const command = new AssembleCommand()
-      command.init()
-
-      expect(command.tasks.length).toBe(4)
+  it('should run tasks successfully', async () => {
+    readProjectConfigurationTask.task.mockImplementation(ctx => {
+      ctx.configuration = {
+        plugins: ['rispa-core'],
+      }
     })
+    createRestorePluginTask.mockImplementation(name => ({
+      title: name,
+      task: restorePluginTask,
+    }))
+
+    const result = await runCommand()
+
+    expect(result).toBeDefined()
+    expect(readProjectConfigurationTask.task).toBeCalled()
+    expect(createRestorePluginTask.mock.calls[0][0]).toBe('rispa-core')
+    expect(cleanCache.task).toBeCalled()
   })
 
-  describe('run', () => {
-    const cwd = '/cwd'
-    const restorePluginTask = jest.fn()
+  it('should throw error working tree has modifications', async () => {
+    mockGit.getChanges.mockImplementation(() => ('changes'))
 
-    it('should run tasks', async () => {
-      readProjectConfigurationTask.task.mockImplementation(ctx => {
-        ctx.configuration = {
-          plugins: ['rispa-core'],
-        }
-      })
-      createRestorePluginTask.mockImplementation(name => ({
-        title: name,
-        task: restorePluginTask,
-      }))
-
-      const command = new AssembleCommand({
-        renderer: 'silent',
-      })
-      command.init()
-      const result = await command.run({
-        cwd,
-        projectPath: cwd,
-      })
-
-      expect(result).toBeDefined()
-      expect(readProjectConfigurationTask.task).toBeCalled()
-      expect(createRestorePluginTask.mock.calls[0][0]).toBe('rispa-core')
+    const command = new AssembleCommand({
+      renderer: 'silent',
     })
+    command.init()
 
-    it('should throw error working tree has modifications', async () => {
-      mockGit.getChanges.mockImplementation(() => ('changes'))
-
-      const command = new AssembleCommand({
-        renderer: 'silent',
-      })
-      command.init()
-
-      await expect(command.run({
-        cwd,
-        projectPath: cwd,
-      })).rejects.toHaveProperty(
+    await expect(runCommand())
+      .rejects.toHaveProperty(
         'message',
         'Working tree has modifications. Cannot restore plugins'
       )
-    })
   })
 })
