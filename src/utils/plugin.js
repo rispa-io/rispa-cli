33d36e784c5cc4a24c05bb069fd48fd772cb91f3
dvpetrov <dvpetrov.main@gmail.com>
2017-06-27 19:54:27 +0300

    Test coverage

--- src/utils/plugin.js
+++ src/utils/plugin.js
@@ -1,6 +1,6 @@
 const path = require('path')
 const fs = require('fs-extra')
-const { PACKAGE_JSON_PATH, DEFAULT_PLUGIN_BRANCH } = require('../constants')
+const { PACKAGE_JSON_PATH, DEFAULT_PLUGIN_BRANCH, PLUGIN_GIT_PREFIX } = require('../constants')
 
 const readPackageJson = pluginPath => {
   const packageJsonPath = path.resolve(pluginPath, PACKAGE_JSON_PATH)
@@ -24,8 +24,32 @@ const parseDependencyVersion = dependencyVersion => {
   return ref
 }
 
+const extractPluginNameFromUrl = cloneUrl => {
+  const parts = cloneUrl.split('/')
+  return parts[parts.length - 1].replace(/\.git$/, '')
+}
+
+const findInList = (plugin, pluginList) => {
+  if (typeof plugin === 'object') {
+    return plugin
+  } else if (plugin.startsWith(PLUGIN_GIT_PREFIX)) {
+    return {
+      name: extractPluginNameFromUrl(plugin),
+      cloneUrl: plugin.replace(PLUGIN_GIT_PREFIX, ''),
+    }
+  }
+
+  const currentPlugin = pluginList.find(({ name, packageName, packageAlias }) =>
+    name === plugin || packageName === plugin || packageAlias === plugin
+  )
+
+  return currentPlugin || { name: plugin }
+}
+
 module.exports = {
   readPackageJson,
   readDependencies,
   parseDependencyVersion,
+  extractPluginNameFromUrl,
+  findInList,
 }
