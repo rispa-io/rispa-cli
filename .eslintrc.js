1c355beab4b8fc38e6859d10bbdb996543a882dc
dvpetrov <dvpetrov.main@gmail.com>
2017-10-12 17:26:03 +0300

    Change eslint, add more complexity

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,7 @@
 module.exports = {
   extends: require.resolve('@rispa/eslint-config'),
   rules: {
+    'complexity': ['error', 10],
     'no-console': 'off',
     'global-require': 'off',
     'import/no-dynamic-require': 'off',
