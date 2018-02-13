583b1c697c90468f8a6f266057f2e31c7e58e982
Vladimir Ratsev <vratsev@gmail.com>
2018-02-13 21:06:47 +0500

    Use plugin ref in assemble command

--- src/tasks/restorePlugin.js
+++ src/tasks/restorePlugin.js
@@ -15,18 +15,18 @@ const createRestorePluginTask = plugin => improveTask({
   task: ctx => {
     const { projectPath, configuration } = ctx
     const pluginsPath = path.resolve(projectPath, configuration.pluginsPath)
-    const { name, remote } = plugin
+    const { name, remote, ref } = plugin
 
     if (checkMode(ctx, DEV_MODE)) {
       if (!fs.existsSync(path.resolve(pluginsPath, name, PACKAGE_JSON_PATH))) {
-        gitCloneRepository(pluginsPath, remote)
+        gitCloneRepository(pluginsPath, remote, ref)
       }
     } else {
       const remotesInstalled = gitGetRemotes(projectPath)
       if (!remotesInstalled[name]) {
         const pluginsRelPath = path.relative(projectPath, pluginsPath)
         const prefix = `${pluginsRelPath}/${name}`
-        gitGetSubtree(projectPath, prefix, name, remote)
+        gitGetSubtree(projectPath, prefix, name, remote, ref)
       }
     }
   },
